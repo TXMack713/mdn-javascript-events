3703ff13f09cbe3058b0865cae5be022d148360c
TXMack713 <mackattack713@gmail.com>
2021-07-23 12:03:44 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello!')
+const btn = document.querySelector('button');
+
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function bgChange() {
+  const rndCol =
+    'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
+
+// function bgChange(e) {
+//   const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+//   e.target.style.backgroundColor = rndCol;
+//   console.log(e);
+// }
+
+btn.addEventListener('click', bgChange);
+
+// btn.onclick = bgChange;
+btn.addEventListener('click', bgChange);
